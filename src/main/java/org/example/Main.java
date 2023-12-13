772dd2a321709693cb2c49969eeba583ffd49f2c
MrHumine <pubh@interia.pl>
2023-12-13 17:44:06 +0000

    Add Array

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,22 @@
 package org.example;
 
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ArrayList<ArrayList> listBoard = new ArrayList<>();
+        ArrayList<Integer> listOfIntegers = new ArrayList<>();
+        for(int i = 0; i < 9; i++){listOfIntegers.add(0);};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        for(int i = 0; i < 9; i++){
+            listBoard.add(listOfIntegers);
+        };
+
+        for(ArrayList word: listBoard){
+            System.out.println(word);
+        };
     }
 }
